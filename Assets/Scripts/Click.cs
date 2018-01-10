c49f0921603ad50cc9e1d515376097adb45b7954
xyskin <xyskin@163.com>
2018-01-10 16:04:21 +0000

    bugs fixed

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drag : MonoBehaviour
+public class Click : MonoBehaviour
 {
     private Transform dragGameObject;
     private LayerMask canDrag;
@@ -11,34 +11,49 @@ public class Drag : MonoBehaviour
     private bool isClickCube;
     private Vector3 targetScreenPoint;
 
-    // Use this for initialization
-    private void Start()
-    {
-        canDrag = 1 << LayerMask.NameToLayer("Cube");
-    }
+    private bool isShow = false;
+    private Vector3 startMarker;
+    private Vector3 endMarker;
+    private float speed = 8.0F;
+    private float startTime;
+    private float journeyLength;
 
+    void Start()
+    {
+
+    }
 
     private void Update()
     {
+        if (isShow)
+        {
+            endMarker = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            endMarker = new Vector3(0, -6, 0);
+        }
 #if UNITY_EDITOR
         //for unity editor
         if (Input.GetMouseButtonDown(0))
         {
             if (CheckGameObjectMouse())
             {
-                offset = dragGameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetScreenPoint.z));
+                startMarker = GameObject.FindGameObjectWithTag("1").transform.position;
+                startTime = Time.time;
+                journeyLength = Vector3.Distance(startMarker, endMarker);
+
             }
         }
 
         if (isClickCube)
         {
-            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetScreenPoint.z);
-
-            Vector3 curWorldPoint = Camera.main.ScreenToWorldPoint(curScreenPoint);
-            dragGameObject.position = curWorldPoint + offset;
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            GameObject.FindGameObjectWithTag("1").transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (GameObject.FindGameObjectWithTag("1").transform.position == endMarker)
         {
             isClickCube = false;
         }
@@ -81,13 +96,19 @@ public class Drag : MonoBehaviour
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo))
             {
-                if (hitInfo.collider.gameObject.tag == "Item")
+                if (hitInfo.collider.gameObject.tag == "UI")
                 {
-                    isClickCube = true;
-                    dragGameObject = hitInfo.collider.gameObject.transform;
-                    targetScreenPoint = Camera.main.WorldToScreenPoint(dragGameObject.position);
-                    return true;
+                    isShow = true;
                 }
+                if (hitInfo.collider.gameObject.tag == "Background")
+                {
+                    isShow = false;
+                }
+
+                isClickCube = true;
+                dragGameObject = hitInfo.collider.gameObject.transform;
+
+                return true;
             }
         }
         return false;
@@ -99,13 +120,20 @@ public class Drag : MonoBehaviour
         RaycastHit hitInfo;
         if (Physics.Raycast(ray, out hitInfo))
         {
-            if (hitInfo.collider.gameObject.tag == "Item")
+            if (hitInfo.collider.gameObject.tag == "UI")
             {
-                isClickCube = true;
-                dragGameObject = hitInfo.collider.gameObject.transform;
-                targetScreenPoint = Camera.main.WorldToScreenPoint(dragGameObject.position);
-                return true;
+                isShow = true;
             }
+            if (hitInfo.collider.gameObject.tag == "Background")
+            {
+                isShow = false;
+            }
+
+            isClickCube = true;
+            dragGameObject = hitInfo.collider.gameObject.transform;
+            Debug.Log(hitInfo.collider.gameObject.name);
+            return true;
+
         }
         return false;
     }
