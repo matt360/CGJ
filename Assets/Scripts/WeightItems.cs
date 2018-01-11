8b4cf3877a47327a7ae1bcec90d0baf0ac25aa04
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-11 17:12:06 +0000

    updat

--- Assets/Scripts/WeightItems.cs
+++ Assets/Scripts/WeightItems.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class WeightItems : MonoBehaviour {
 
-	public Transform weightingPlace;
+	public Transform Beaker;
+    public Transform Bottle;
+    public Transform Erl_Flask;
+    public Transform Filter;
+    public Transform Measuring_Cylinder;
+    public Transform Scale;
+    public Transform Spatula;
+    public Transform Squeeze_Bottle;
+    public Transform Flask_Small;
+    public Transform Separate_Layers;
+    public Transform Ring_Stand;
+    public Transform Round_Bottomed_Flask;
 
     void OnTriggerStay(Collider collision)
     { 
-        collision.gameObject.transform.position = weightingPlace.transform.position;
-    }
+        if (collision.tag == "Beaker")
+            collision.gameObject.transform.position = Beaker.transform.position;
+
+       
+}
 
 
 }
