5094f40c473342b32e5596e9a38c72287460f822
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-11 15:31:18 +0000

    scale prefab update

---
+++ Assets/Scripts/WeightItems.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightItems : MonoBehaviour {
+
+	public Transform weightingPlace;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
