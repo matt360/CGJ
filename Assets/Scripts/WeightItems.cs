cd7b08b05f08f95773ca5df593b524bab40469c7
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-11 17:18:41 +0000

    separate transforms

--- Assets/Scripts/WeightItems.cs
+++ Assets/Scripts/WeightItems.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeightItems : MonoBehaviour {
+public class WeightItems : MonoBehaviour
+{
 
-	public Transform Beaker;
+    public Transform Beaker;
     public Transform Bottle;
     public Transform Erl_Flask;
     public Transform Filter;
     public Transform Measuring_Cylinder;
-    public Transform Scale;
     public Transform Spatula;
     public Transform Squeeze_Bottle;
     public Transform Flask_Small;
@@ -18,12 +18,53 @@ public class WeightItems : MonoBehaviour {
     public Transform Round_Bottomed_Flask;
 
     void OnTriggerStay(Collider collision)
-    { 
-        if (collision.tag == "Beaker")
-            collision.gameObject.transform.position = Beaker.transform.position;
+    {
+        switch (collision.tag)
+        {
+            case "Beaker":
+                collision.gameObject.transform.position = Beaker.transform.position;
+                break;
 
-       
-}
+            case "Bottle":
+                collision.gameObject.transform.position = Bottle.transform.position;
+                break;
 
+            case "Erl_Flask":
+                collision.gameObject.transform.position = Erl_Flask.transform.position;
+                break;
+
+            case "Filter":
+                collision.gameObject.transform.position = Filter.transform.position;
+                break;
+
+            case "Measuring_Cylinder":
+                collision.gameObject.transform.position = Measuring_Cylinder.transform.position;
+                break;
+
+            case "Spatula":
+                collision.gameObject.transform.position = Spatula.transform.position;
+                break;
+
+            case "Squeeze_Bottle":
+                collision.gameObject.transform.position = Squeeze_Bottle.transform.position;
+                break;
+
+            case "Flask_Small":
+                collision.gameObject.transform.position = Flask_Small.transform.position;
+                break;
+
+            case "Separate_Layers":
+                collision.gameObject.transform.position = Separate_Layers.transform.position;
+                break;
+
+            case "Ring_Stand":
+                collision.gameObject.transform.position = Ring_Stand.transform.position;
+                break;
+
+            case "Round_Bottomed_Flask":
+                collision.gameObject.transform.position = Round_Bottomed_Flask.transform.position;
+                break;
+        }
+    }
 
 }
