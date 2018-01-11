6c6ebd9dfb92a2a8111bcee64f9ea6736cf89a23
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-11 11:52:14 +0000

    pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -38,5 +39,11 @@ public class PauseMenu : MonoBehaviour {
 
         Time.timeScale = 0.0f;
         gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
     }
 }
