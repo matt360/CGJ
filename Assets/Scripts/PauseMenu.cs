b97daa899696e1aad52e6e7ada87d691b97809d1
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-11 11:21:26 +0000

    pause button on esacpe press

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // static accessible to other scripts
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
 	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1.0f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0.0f;
+        gameIsPaused = true;
+    }
 }
