62a4b9534c724ad552c699175aee06f7ae6fab29
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-11 16:24:15 +0000

    beaker fixed

--- Assets/Scripts/WeightItems.cs
+++ Assets/Scripts/WeightItems.cs
@@ -7,23 +7,9 @@ public class WeightItems : MonoBehaviour {
 	public Transform weightingPlace;
     public Transform itemWeightingPlace;
 
-    //void OnCollisionEnter(Collision coll) {
-    //	if (coll.gameObject.tag == "Item")
-    //		coll.gameObject.transform.position = weightingPlace.transform.position;
-    //	Debug.Log ("collision happened");
-    //}
-
     void OnTriggerStay(Collider collision)
     { 
-        //foreach (ContactPoint contact in collision.contacts)
-        //{
-        //    collision.transform.position = weightingPlace.transform.position;
-        //}
-        //if (collision.gameObject.tag == "Item")
         collision.gameObject.transform.position = weightingPlace.transform.position;
-
-
-        Debug.Log("Collision happened");
     }
 
 
